87578dfae18882f5e83429a0bfbdd8670b943433
MaximTuktarev <maxim.tuktarev@mail.ru>
2022-03-17 19:48:58 +0400

    init tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,9 +1,47 @@
 package ru.netology.service;
 
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class CashbackHackServiceTest {
 
+    CashbackHackService cashback = new CashbackHackService();
+
+    @Test
+
+    public void oneThousand() {
+        int expected = 1000;
+        int actual = cashback.remain(1000);
+        assertEquals(actual, expected);
+    }
 
 
+    @Test
+    public void Half() {
+        int expected = 500;
+        int actual = cashback.remain(500);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void trista() {
+        int expected = 300;
+        int actual = cashback.remain(700);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void border999() {
+        int expected = 999;
+        int actual = cashback.remain(1001);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void minimumOne() {
+        int expected = 1 ;
+        int actual = cashback.remain(999);
+        assertEquals(actual, expected);
+    }
 }
